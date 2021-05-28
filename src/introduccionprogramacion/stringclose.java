9f35e0577a6431bded341f6a43beaab5a5154934
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-05-27 21:58:11 -0500

    string close

---
+++ src/introduccionprogramacion/stringclose.java
@@ -0,0 +1,119 @@
+package introduccionprogramacion;
+
+import java.util.Scanner;
+
+public class stringclose {
+	
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {    
+		
+		String cadena; 
+		Scanner entradacadena;
+		
+		
+        System.out.println("Ingresa la cadena a evaluar");
+        entradacadena = new Scanner (System.in);
+        cadena =  entradacadena.nextLine();
+		
+		
+		if(  cadena.length() > 0 ) {
+			
+			if( cadena.contains("{") || cadena.contains("}")  ) {
+				
+				// System.out.println(  "si tiene llaves" );
+				
+				cerrarllaves( cadena );
+				
+			}else{
+				
+				System.out.println(  "La cadena no contiene llaves" );
+			}		
+			
+		}else {
+			System.out.println(  "La cadena esta vacia" );
+		}
+		
+	}
+	
+	
+	
+	public static void cerrarllaves( String cadena ){
+		
+		// System.out.println(  cadena.length() );
+		
+		Integer abrircount = 0;
+		Integer cerrarcount = 0;
+		Integer diferencia;
+		String nuevacadena, cadenatemp1 = "", caracter = "",   cadenatemp = cadena;
+		String abrirchar = "", cerrarchar = "";
+		
+
+		
+		for( Integer i = 0; i < cadena.length(); i++ ) {
+
+
+			//Se evalua el numero de llaves abiertas
+			if( cadena.charAt(i) ==  '{' ) {
+				abrircount++;
+			//	cadenatemp1 += cadena.charAt(i) + "}";
+			}
+			
+			
+			
+			if( cadena.charAt(i) == '}' ) {
+				
+				cerrarcount++;
+				//cadenatemp1 +=   "{"  + cadena.charAt(i);  
+			}
+			
+			
+		}
+		
+		System.out.println( "+++++++++++++++++++++++++++" );
+		System.out.println( cadenatemp1 );
+		//System.out.println( caracter );
+		//System.out.println( "+++++++++++++++++++++++++++" );
+		
+		
+		if(  abrircount > cerrarcount ) {
+			diferencia = abrircount - cerrarcount;
+			//System.out.println(  diferencia );
+			
+			
+			for( Integer i = 0; i < diferencia; i++  ) {
+				cerrarchar += "}";
+			}
+			
+			nuevacadena = cadenatemp + cerrarchar;
+			
+			System.out.println( nuevacadena  );
+			
+			System.out.println(  "Se agregaron la cantidad " +  diferencia +  "  de llave(s) de }" );
+			
+		}
+		
+		
+		
+		if(  cerrarcount > abrircount ) {
+			diferencia = cerrarcount - abrircount;
+			//System.out.println(  diferencia );
+			
+			for( Integer i = 0; i < diferencia; i++  ) {
+				abrirchar += "{";
+				
+			}
+			
+			nuevacadena = abrirchar + cadenatemp;	
+			System.out.println( nuevacadena  );
+			System.out.println(  "Se agregaron la cantidad " +  diferencia  +  "  de llave(s) de { " );
+			
+		}
+		
+		
+		
+		
+	}
+	
+	
+
+}

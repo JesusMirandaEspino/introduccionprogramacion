2bfb7586d7a9cf1664d94d3f47a74d5a54b6c7e6
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-06-04 23:11:31 -0500

    refactoring code

--- src/introduccionprogramacion/stringclose.java
+++ src/introduccionprogramacion/stringclose.java
@@ -12,7 +12,6 @@ public class stringclose {
 		String cadena; 
 		Scanner entradacadena;
 		
-		
         System.out.println("Ingresa la cadena a evaluar");
         entradacadena = new Scanner (System.in);
         cadena =  entradacadena.nextLine();
@@ -23,10 +22,7 @@ public class stringclose {
 			if( cadena.contains("{") || cadena.contains("}")  ) {
 				
 				// System.out.println(  "si tiene llaves" );
-				
 				cerrarllaves( cadena );
-				
-				
 				
 				if( valor  ) {
 					System.out.println("La cadena es valida");
@@ -45,36 +41,23 @@ public class stringclose {
 	
 	
 	
+	
 	public static void cerrarllaves( String cadena ){
-		
 		// System.out.println(  cadena.length() );
-		
-		Integer abrircount = 0;
-		Integer cerrarcount = 0;
-		Integer diferencia;
-		String nuevacadena, cadenatemp1 = "", caracter = "",   cadenatemp = cadena;
-		String abrirchar = "", cerrarchar = "";
-		
-
+		Integer abrircount = 0,  cerrarcount = 0,  diferencia;
+		String nuevacadena, cadenatemp1 = "", caracter = "",   cadenatemp = cadena,  abrirchar = "", cerrarchar = "";
 		
 		for( Integer i = 0; i < cadena.length(); i++ ) {
-
-
 			//Se evalua el numero de llaves abiertas
 			if( cadena.charAt(i) ==  '{' ) {
 				abrircount++;
 			//	cadenatemp1 += cadena.charAt(i) + "}";
 			}
 			
-			
-			
 			if( cadena.charAt(i) == '}' ) {
-				
 				cerrarcount++;
 				//cadenatemp1 +=   "{"  + cadena.charAt(i);  
 			}
-			
-			
 		}
 		
 		System.out.println( "+++++++++++++++++++++++++++" );
@@ -82,11 +65,9 @@ public class stringclose {
 		//System.out.println( caracter );
 		//System.out.println( "+++++++++++++++++++++++++++" );
 		
-		
 		if(  abrircount > cerrarcount ) {
 			diferencia = abrircount - cerrarcount;
 			//System.out.println(  diferencia );
-			
 			
 			for( Integer i = 0; i < diferencia; i++  ) {
 				cerrarchar += "}";
@@ -94,22 +75,16 @@ public class stringclose {
 			
 			nuevacadena = cadenatemp + cerrarchar;
 			
-			
 			System.out.println( "+++++++++++++++++++++++++++" );
 			System.out.println("La cadena ingresada no es valida");
 			System.out.println( "+++++++++++++++++++++++++++" );
 			
-			
 			System.out.println( "Posible solucion"  );
-			
 			System.out.println( nuevacadena  );
-			
 			System.out.println(  "Se deben agregar la cantidad " +  diferencia +  "  de llave(s) de }" );
 			
 			valor = false;
-			
 		}
-		
 		
 		
 		if(  cerrarcount > abrircount ) {
@@ -118,7 +93,6 @@ public class stringclose {
 			
 			for( Integer i = 0; i < diferencia; i++  ) {
 				abrirchar += "{";
-				
 			}
 			
 			nuevacadena = abrirchar + cadenatemp;
@@ -135,11 +109,6 @@ public class stringclose {
 			
 		}
 		
-		
-		
-		
 	}
 	
-	
-
 }
